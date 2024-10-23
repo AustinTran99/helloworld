798b5280e86f0922d5e45d3f7eaa3b4ff3f64c47
Austin Tran <austintran99@yahoo.com>
2024-10-22 18:34:44 -0700

    print date

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,5 +7,6 @@ public class Main {
         String name = "Austin";
         System.out.println("Hello World!");
         System.out.println(name);
+        System.out.println(new Date());
     }
 }
