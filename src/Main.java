2c0aaf48e9e1a126f881baf7c2de94678aa26dfc
Austin Tran <austintran99@yahoo.com>
2024-10-22 18:32:40 -0700

    Added name variable

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String name = "Austin";
         System.out.println("Hello World!");
+        System.out.println(name);
     }
 }
